6f881333e7948e19c4d5f92f94288b69ecde4449
Mrrwmix <seidoc@gmail.com>
2019-07-26 23:06:41 -0700

    Creating array of objects

---
+++ app/data/friends.js
@@ -0,0 +1,62 @@
+var wrestlers = [
+    {
+        "name":"Minoru Suzuki",
+        "photo":"",
+        "scores":[3,1,3,1,4,5,2,2,5,3]
+    },
+    {
+        "name":"Io Shirai",
+        "photo":"",
+        "scores":[2,2,4,2,3,3,3,3,5,4]
+    },
+    {
+        "name":"Kairi",
+        "photo":"",
+        "scores":[3,4,3,4,2,2,4,3,3,5]
+    },
+    {
+        "name":"Okada",
+        "photo":"",
+        "scores":[5,4,5,4,4,2,4,4,3,3]
+    },
+    {
+        "name":"Orange Cassidy",
+        "photo":"",
+        "scores":[1,5,5,3,3,1,4,1,1,1]
+    },
+    {
+        "name":"Will Ospreay",
+        "photo":"",
+        "scores":[3,3,3,4,3,1,5,2,4,5]
+    },
+    {
+        "name":"Kevin Owens",
+        "photo":"",
+        "scores":[5,1,4,1,2,4,1,5,5,2]
+    },
+    {
+        "name":"John Cena",
+        "photo":"",
+        "scores":[2,4,5,4,4,1,4,5,1,2]
+    },
+    {
+        "name":"Stone Cold Steve Austin",
+        "photo":"",
+        "scores":[2,1,5,1,3,3,1,5,5,3]
+    },
+    {
+        "name":"The Rock",
+        "photo":"",
+        "scores":[4,3,5,2,5,2,3,5,3,3]
+    },
+    {
+        "name":"Jericho",
+        "photo":"",
+        "scores":[4,3,5,3,5,3,2,5,3,4]
+    },
+    {
+        "name":"Pentagon Jr.",
+        "photo":"",
+        "scores":[3,2,4,2,4,4,3,3,4,4]
+    }
+]
